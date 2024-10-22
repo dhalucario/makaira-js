8bfe7c2bbc44f0591e293bec56416dd3dc173cfe
Leon Grünewald <10533763+dhalucario@users.noreply.github.com>
2024-10-22 13:38:08 +0200

    Dont use object for filter

--- src/client.ts
+++ src/client.ts
@@ -3,18 +3,26 @@ export default class Client {
   instanceId: string;
   bearerToken: string;
 
-  constructor(customer: string, instanceId: string, bearerToken: string) {
+  constructor({customer, instanceId, bearerToken}: {customer: string, instanceId: string, bearerToken: string}) {
     this.customer = customer;
     this.instanceId = instanceId;
     this.bearerToken = bearerToken;
   }
 
-  async filter(sort = "id", direction = 'ASC', offset = null, count = 10000) {
+  async filter(sort: string = "id", direction: string = "ASC", count: number = 10000, offset?: string) {
     const headers = new Headers();
     headers.set('X-Makaira-Instance', this.instanceId);
     headers.set('Content-Type', 'application/json');
-    const res = await fetch(`https://${this.customer}/filter`, {
-      headers
+    headers.set('Authorization', `Bearer ${this.bearerToken}`);
+    const res = await fetch(`https://${this.customer}.makaira.io/filter`, {
+      headers,
+      body: JSON.stringify({
+        sort,
+        direction,
+        offset: offset ?? 0,
+        count
+      }),
+      method: "POST"
     });
     return await res.json();
   }
